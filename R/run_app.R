c64d7db9e4e0be3b6730a3a9ae2df33313503881
divyansh997 <code.divyansh@gmail.com>
2020-08-25 08:53:04 +0530

    App now launches directly in browser.

--- R/run_app.R
+++ R/run_app.R
@@ -8,7 +8,8 @@
 
 run_app <- function(...) {
   with_golem_options(app = shinyApp(ui = app_ui,
-                                    server = app_server),
+                                    server = app_server,
+                                    options = list(launch.browser = TRUE)),
                      golem_opts = list(...))
 }
 
