780ea472ffc7a417626043bee9607829794ab844
Divyansh Chawla <code.divyansh@gmail.com>
2021-01-20 17:33:17 +0530

    Change command for running application

--- R/run_app.R
+++ R/run_app.R
@@ -6,7 +6,7 @@
 #' @importFrom shiny shinyApp
 #' @importFrom golem with_golem_options
 
-run_app <- function(...) {
+run_rsqliteadmin <- function(...) {
   with_golem_options(app = shinyApp(ui = app_ui,
                                     server = app_server,
                                     options = list(launch.browser = TRUE)),
