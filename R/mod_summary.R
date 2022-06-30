414ac7ef47e0d547c3b098d968db5b0741d3f80c
Anushka7310 <anushkaguptably@gmail.com>
2022-06-30 14:40:07 +0530

    added scroller using dt

--- R/mod_summary.R
+++ R/mod_summary.R
@@ -1,28 +1,46 @@
+#' view_tables UI Function
+#'
+#' @description Shiny module for View/Edit Tables tab.
+#'
+#' @param id,input,output,session Internal parameters for {shiny}.
+#'
+#' @noRd
+#'
+#' @importFrom shiny NS
+#' @importFrom DT DTOutput
+#' @importFrom DT renderDT
+#' @importFrom DT datatable
+#' @importFrom RSQLite dbGetQuery
+#' @importFrom RSQLite dbExecute
+
+
 mod_summary_ui <- function(id) {
   ns <- NS(id)
 
-  # Header - Display rows from the start.
-  # Footer - Display rows from the end.
-
   tabPanel(title = "Summary",
            column(width = 12,
-                  fluidRow(column(
-                    width = 12,
-                    h2(textOutput(ns("heading"))),
-                    DT::DTOutput(ns("anushka"))
-                  ))))
+                  fluidRow(
+                    column(width = 12,
+                           h2(textOutput(ns(
+                             "heading"
+                           ))),
+                           br(),
+                           DT::DTOutput(ns("anushka")))
+                  )))
 
 }
+
+#' summary Server Function
+#'
+#' @noRd
+
+
 mod_summary_server <-
   function(input,
            output,
            session,
            conn) {
     ns <- session$ns
-
-
-
-
 
     table_info <- reactiveValues(
       column_names = NULL,
@@ -33,6 +51,7 @@ mod_summary_server <-
       number_rows = 1000,
       offset = 0
     )
+
     output$heading <-
       renderText({
         paste0("Showing Summary of ", conn$active_table)
@@ -43,10 +62,14 @@ mod_summary_server <-
                                   data_fetch_query(conn$active_table,
                                                    3000,
                                                    0))
-      DT::datatable(data.frame(unclass(summary(data)), row.names=NULL, check.names = FALSE, stringsAsFactors = FALSE),
-                    extensions = 'Buttons', options = list(
-                      dom = 'Bfrtip',
-                      buttons = c('copy', 'csv', 'excel', 'pdf', 'print')
-                    ))
+      DT::datatable(
+        data.frame(
+          unclass(summary(data)),
+          row.names = NULL,
+          check.names = FALSE,
+          stringsAsFactors = FALSE
+        ),
+        options = list(paging = FALSE, scroller = TRUE, scrollX = T)
+      )
     })
   }
